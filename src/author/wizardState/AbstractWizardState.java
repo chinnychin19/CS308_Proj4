55384c75f7f864347e9642091a7c885293aa30d8
Tyler Nisonoff <tylernisonoff@gmail.com>
2013-11-04 23:53:06 -0500

    added gif

--- src/author/wizardState/AbstractWizardState.java
+++ src/author/wizardState/AbstractWizardState.java
@@ -1,5 +1,8 @@
 package author.wizardState;
 
 public abstract class AbstractWizardState {
+	
 	public abstract void generateXML();
+	
+	public abstract String getFieldValue(String label);
 }
--- src/game/model/Model.java
+++ src/game/model/Model.java
@@ -3,5 +3,5 @@ package game.model;
 import java.util.Collection;
 
 public abstract class Model {
-	 public abstract Collection<AbstractViewableObject> getViewableObjects();
+
 }
