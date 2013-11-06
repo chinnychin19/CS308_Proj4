2e3da2629dfabf478e85d872582ee28e94d92ef1
Austin Ness <austinness@gmail.com>
2013-11-06 15:42:15 -0500

    a

--- src/game/controller/WorldController.java
+++ src/game/controller/WorldController.java
@@ -6,5 +6,5 @@ import game.model.AbstractViewableObject;
 import game.model.Model;
 
 public abstract class WorldController extends AbstractController{
-	public abstract Collection<AbstractViewableObject> getViewableObjects();
+        public abstract Collection<AbstractViewableObject> getViewableObjects();
 }
