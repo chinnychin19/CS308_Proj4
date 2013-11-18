9fa3dcc8b782eac4d11129d3a16670cfa40ea10a
mray90 <matthew_ray90@bellsouth.net>
2013-11-17 20:31:59 -0500

    Created ImagePanel to fit into AbstractWizardPanel inheritance hierarchy; added FileChooserSingleton for other classes to use

---
+++ src/author/FileChooserSingleton.java
@@ -0,0 +1,20 @@
+package author;
+
+import javax.swing.JFileChooser;
+
+public class FileChooserSingleton extends JFileChooser {
+
+    private static FileChooserSingleton instance;
+
+    private FileChooserSingleton(){
+        super("./");
+    }
+
+    public static synchronized FileChooserSingleton getInstance()
+    {
+        if (instance == null)
+            instance = new FileChooserSingleton();
+
+        return instance;
+    }
+}
