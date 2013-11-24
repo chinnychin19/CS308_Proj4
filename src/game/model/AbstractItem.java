4e98c6bdbfa90c9dc692014d8d3ca0bbe44b5d59
Ryan Toussaint <ryan.toussaint@duke.edu>
2013-11-23 22:22:14 -0500

    Javadocing AbstractCharacter, AbstractGround, and AbstractItem with refactoring

--- src/game/model/AbstractItem.java
+++ src/game/model/AbstractItem.java
@@ -1,8 +1,12 @@
 package game.model;
 
-import org.json.simple.JSONObject;
 import util.jsonwrapper.SmartJsonObject;
 
+/**
+ * Contains the basic information for an item and extrends AbstractModelOjbject
+ * @author tylernisonoff
+ *
+ */
 public abstract class AbstractItem extends AbstractModelObject {
 
     public AbstractItem (GameModel model, SmartJsonObject definition) {
