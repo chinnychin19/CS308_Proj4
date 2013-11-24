2fab73aedd8fc520a9e9e6810295940e3d70b8fd
Ryan Toussaint <ryan.toussaint@duke.edu>
2013-11-23 22:40:17 -0500

    Java docing for Abstract Classes in model package

--- src/game/model/AbstractModelObject.java
+++ src/game/model/AbstractModelObject.java
@@ -1,20 +1,24 @@
 package game.model;
 
-import org.json.simple.JSONObject;
 import util.jsonwrapper.SmartJsonObject;
 import util.jsonwrapper.jsonexceptions.SmartJsonException;
 import constants.Constants;
 
+/**
+ * Creates a new instance of a Model Object which has a name and a model attached to it.
+ * @author tylernisonoff
+ *
+ */
 
 public class AbstractModelObject {
     private String myName;
     private GameModel myModel;
 
     protected AbstractModelObject (GameModel model) {
-        myName = "";
+        myName = Constants.EMPTY_STRING;
         myModel = model;
     }
-
+   
     public AbstractModelObject (GameModel model, SmartJsonObject definition) {
         myModel = model;
         try {
@@ -25,14 +29,25 @@ public class AbstractModelObject {
         }
     }
     
+    /**
+     * Gets the model that is attached to this object
+     * @return the model
+     */
     public GameModel getModel(){
         return myModel;
     }
-
+    /**
+     * Gets the name of this object
+     * @return the name
+     */
     public String getName () {
         return myName;
     }
 
+    /**
+     * Set the name for this object
+     * @param name the name of the object
+     */
     protected void setName (String name) {
         myName = name;
     }
--- src/game/model/AbstractViewableObject.java
+++ src/game/model/AbstractViewableObject.java
@@ -8,7 +8,12 @@ import util.jsonwrapper.SmartJsonObject;
 import util.jsonwrapper.jsonexceptions.SmartJsonException;
 import constants.Constants;
 
-
+/**
+ * Abstract class for viewable objects to implement.  Contains the functionality associated with whether or not an object can 
+ * step on this viewable object -- Default is false.
+ * @author tylernisonoff
+ *
+ */
 public abstract class AbstractViewableObject extends AbstractViewable {
 
     public AbstractViewableObject (GameModel model,
