5ad96041b7709b45e5423d04cb5c27663d4ec98d
Westen Koorbusch <wes.koorbusch@gmail.com>
2013-12-02 23:00:33 -0500

    added untracked files

---
+++ src/author/LevelEditorPanel.java
@@ -0,0 +1,16 @@
+package author;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class LevelEditorPanel extends JPanel {
+
+    public LevelEditorPanel () {
+        super();
+        this.setBackground(Color.BLUE);
+    }
+
+}
