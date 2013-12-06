af04e1d50709585897842e831aecba4f053fff8f
Chinmay Patwardhan <chinnychin19@gmail.com>
2013-12-06 18:33:03 -0500

    made an AbstractEnemyParty class

---
+++ src/game/model/battle/AbstractEnemyParty.java
@@ -0,0 +1,31 @@
+package game.model.battle;
+
+import java.util.List;
+import game.controller.GameController;
+import game.model.Fighter;
+import game.model.Monster;
+import game.model.attack.Attack;
+
+public abstract class AbstractEnemyParty extends AbstractBattleParty {
+
+    public AbstractEnemyParty (GameController controller, Fighter fighter) {
+        super(controller, fighter);
+    }
+    public AbstractEnemyParty (GameController controller, Monster monster) {
+        super(controller, monster);
+    }
+
+    @Override
+    public void doTurn () {
+        Attack attack = getAttack();
+        getBattle().attackPlayer(attack);
+        super.doTurn();
+    }
+
+    private Attack getAttack () {
+        List<Attack> attacks = getCurrentMonster().getAllAvailableAttacks();
+        int rand = (int) (Math.random() * attacks.size());
+        return attacks.get(rand);
+    }
+
+}
--- src/game/model/battle/NPCParty.java
+++ src/game/model/battle/NPCParty.java
@@ -6,23 +6,9 @@ import game.model.Fighter;
 import game.model.FightingNPC;
 import game.model.attack.Attack;
 
-public class NPCParty extends AbstractBattleParty {
+public class NPCParty extends AbstractEnemyParty {
 
     public NPCParty (GameController controller, FightingNPC enemy) {
         super(controller, enemy);
     }
-
-    @Override
-    public void doTurn () {
-        Attack attack = getAttack();
-        getBattle().attackPlayer(attack);
-        super.doTurn();
-    }
-
-    public Attack getAttack () {
-        List<Attack> attacks = getCurrentMonster().getAllAvailableAttacks();
-        int rand = (int) (Math.random() * attacks.size());
-        return attacks.get(rand);
-    }
-
 }
